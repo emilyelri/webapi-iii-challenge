3ea7af4a280c97c8ae70f422fe9eb023ef4cadf0
Emily_Richard <richard.emily.e@gmail.com>
2019-10-30 14:29:40 -0400

    started server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code away!
+const express = require('express');
+const server = express();
+// const router = require('./router.js');
+const port = 8000;
+
+server.use(express.json());
+// server.use('/api/posts', router);
+
+server.listen(port, () => {
+  console.log(`~~~ listening on port ${port}  ~~~`)
+});
